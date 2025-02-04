505dfe3d16323574e89b2781440430734fc2dce3
ityeri <yeongbin0909@naver.com>
2025-02-04 15:01:51 +0900

    각 영역의 종류를 정의하는 열거형 추가

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/area/AreaType.kt
@@ -0,0 +1,49 @@
+package com.github.it.tangTaMeokGi.area
+
+import com.github.it.tangTaMeokGi.area.areaState.EmptyAreaState
+
+enum class AreaType {
+    EMPTY_AREA {
+        override fun setTypeThis(area: Area) {
+            area.state = EmptyAreaState(area)
+        }
+    },
+
+
+
+    GENERAL_AREA {
+        override fun setTypeThis(area: Area) {
+            TODO("Not yet implemented")
+        }
+    },
+    EFFECT_AREA {
+        override fun setTypeThis(area: Area) {
+            TODO("Not yet implemented")
+        }
+    },
+
+
+
+    PUBLIC_AREA {
+        override fun setTypeThis(area: Area) {
+            TODO("Not yet implemented")
+        }
+    },
+
+
+
+    WAR_GENERAL_AREA {
+        override fun setTypeThis(area: Area) {
+            TODO("Not yet implemented")
+        }
+    },
+    WAR_EFFECT_AREA {
+        override fun setTypeThis(area: Area) {
+            TODO("Not yet implemented")
+        }
+    };
+
+
+
+    abstract fun setTypeThis(area: Area)
+}
