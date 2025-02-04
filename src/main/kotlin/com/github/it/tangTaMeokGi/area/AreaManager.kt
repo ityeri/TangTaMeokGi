4634a6b0cd6fa7e84c96ab41892db03fd1e361c1
ityeri <yeongbin0909@naver.com>
2025-02-04 15:07:09 +0900

    새 area 시스템에 맞춘 Area 객체와 AreaManager 객체 작성

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/area/AreaManager.kt
@@ -0,0 +1,43 @@
+package com.github.it.tangTaMeokGi.area
+
+import org.bukkit.World
+
+class AreaManager(
+    val world: World,
+    val mapWidth: Int,
+    val mapDepth: Int,
+    val areaWidth: Int,
+    val areaDepth: Int
+) {
+
+    private val areaMap: MutableList<MutableList<BaseArea>> = MutableList(mapDepth) { mutableListOf() }
+
+    init {
+        for (z in 0 until  mapDepth) {
+            val currentLine = areaMap.get(z)
+            for (x in 0 until  mapWidth) {
+                currentLine.add(BaseArea(world,
+                    x*areaWidth, z*areaDepth, areaDepth, areaDepth
+                ))
+            }
+        }
+    }
+
+
+    fun generate() {
+
+    }
+
+
+    fun getArea(x: Int, z: Int): BaseArea? {
+        try {
+            return areaMap[z][x]
+        } catch (e: IndexOutOfBoundsException) {
+            return null
+        }
+    }
+
+    fun setArea(x: Int, z: Int, newArea: BaseArea) {
+        areaMap[z][x] = newArea
+    }
+}
