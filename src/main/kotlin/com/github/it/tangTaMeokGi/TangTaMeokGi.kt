1f5d771963732675d3172dd7f83e42ce7f607131
ityeri <yeongbin0909@naver.com>
2025-02-04 15:07:31 +0900

    nothing

--- src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
@@ -1,14 +1,15 @@
 package com.github.it.tangTaMeokGi
 
-import com.github.it.tangTaMeokGi.area.BaseArea
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
+import org.bukkit.block.BlockState
 import org.bukkit.plugin.java.JavaPlugin
 
 class TangTaMeokGi : JavaPlugin() {
 
     override fun onEnable() {
         Bukkit.getServer().sendMessage(Component.text("탕타묵기"))
+
     }
 
     override fun onDisable() {
