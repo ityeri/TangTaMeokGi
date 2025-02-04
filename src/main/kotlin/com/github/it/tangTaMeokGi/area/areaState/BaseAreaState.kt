5788a1ec54522cb45aa595346e4062025cc9f4b2
ityeri <yeongbin0909@naver.com>
2025-02-04 15:06:16 +0900

    기본적인 AreaState 작성

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/area/areaState/BaseAreaState.kt
@@ -0,0 +1,7 @@
+package com.github.it.tangTaMeokGi.area.areaState
+
+import com.github.it.tangTaMeokGi.area.Area
+
+open class BaseAreaState(
+    val area: Area
+)
---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/area/areaState/EmptyAreaState.kt
@@ -0,0 +1,8 @@
+package com.github.it.tangTaMeokGi.area.areaState
+
+import com.github.it.tangTaMeokGi.area.Area
+
+class EmptyAreaState(
+    area: Area
+) : BaseAreaState(area) {
+}
