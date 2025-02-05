cc1715906bfc33bb638bb1abdb3bbd8337da38c8
ityeri <yeongbin0909@naver.com>
2025-02-05 13:15:00 +0900

    areaManager 테스트 코드 추가

--- src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
@@ -1,14 +1,19 @@
 package com.github.it.tangTaMeokGi
 
+import com.github.it.tangTaMeokGi.area.Area
+import com.github.it.tangTaMeokGi.area.AreaManager
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.block.BlockState
 import org.bukkit.plugin.java.JavaPlugin
 
 class TangTaMeokGi : JavaPlugin() {
+    val areaManager: AreaManager = AreaManager(Bukkit.getWorld("world")!!,
+        8, 8, 8, 8)
 
     override fun onEnable() {
         Bukkit.getServer().sendMessage(Component.text("탕타묵기"))
+        areaManager.generate()
 
     }
 
