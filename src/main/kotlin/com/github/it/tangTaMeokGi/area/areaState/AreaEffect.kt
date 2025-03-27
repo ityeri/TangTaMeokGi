7dd2268290a2fe7e8679f180b781761181f799e5
it <yeongbin0909@naver.com>
2025-03-27 20:24:58 +0900

    효과 개열 땅의 포션 이팩트를 관리하는 간단한 데이터 클래스 추가

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/area/areaState/AreaEffect.kt
@@ -0,0 +1,5 @@
+package com.github.it.tangTaMeokGi.area.areaState
+
+import org.bukkit.potion.PotionEffect
+
+data class AreaEffect(val buffEffect: PotionEffect? = null, val debuffEffect: PotionEffect? = null)
