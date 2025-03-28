002ad4fac7d4f05abf01cfaa5d225eaa6fa6bb1a
it <yeongbin0909@naver.com>
2025-03-28 13:55:40 +0900

    영역 공격 테스트용 코드 추가

--- src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
@@ -4,8 +4,11 @@ import com.github.it.tangTaMeokGi.area.Area
 import com.github.it.tangTaMeokGi.area.AreaManager
 import com.github.it.tangTaMeokGi.area.areaState.PublicAreaState
 import com.github.it.tangTaMeokGi.game.GameManager
+import com.github.it.tangTaMeokGi.game.team.Team
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
+import org.bukkit.Color
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
@@ -14,19 +17,24 @@ import org.bukkit.plugin.java.JavaPlugin
 class TangTaMeokGi : JavaPlugin(), Listener {
     val gameManager = GameManager(this)
 
-    val areaManager: AreaManager = AreaManager(gameManager, 16, 1)
-    val testArea: Area
+    lateinit var testPlayer: Player
+    lateinit var testArea: Area
 
-    init {
-        areaManager.generate()
-        testArea = areaManager.getArea(1, 1)!!
-        testArea.state = PublicAreaState(testArea, false)
-    }
 
     override fun onEnable() {
         Bukkit.getServer().sendMessage(Component.text("탕타묵기"))
-//        areaManager.mapGenerate()
-        testArea.enable()
+
+        gameManager.init(
+            mapSize = 4, areaSize = 4, gameTimeMin = 1
+        )
+
+        testArea = gameManager.areaManager!!.getArea(1, 1)!!
+
+        gameManager.teamManager!!.addTeam(
+            Team("test", "test", Color.RED)
+        )
+
+        gameManager.teamManager!!.getTeam("test")!!.addPlayer(testPlayer)
     }
 
     override fun onDisable() {
