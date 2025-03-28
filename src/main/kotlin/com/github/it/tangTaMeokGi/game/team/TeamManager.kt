f30d7050bfb67e5348f18613e73e6717567e2386
it <yeongbin0909@naver.com>
2025-03-28 13:38:10 +0900

    팀 매니저 클래스 추가

--- src/main/kotlin/com/github/it/tangTaMeokGi/game/team/TeamManager.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/game/team/TeamManager.kt
@@ -1,5 +1,25 @@
 package com.github.it.tangTaMeokGi.game.team
 
-class TeamManager {
+import org.bukkit.entity.Player
 
+class TeamManager {
+    private val teamStore: MutableMap<String, Team> = mutableMapOf()
+
+    fun getTeam(id: String): Team? {
+        return teamStore.get(id)
+    }
+    fun getTeam(player: Player): Team? {
+        for (team in teamStore.values) {
+
+            if (player.uniqueId in team.playerUUIDs) {
+                return team
+            }
+        }
+
+        return null
+    }
+
+    fun addTeam(team: Team) {
+        teamStore[team.id] = team
+    }
 }
