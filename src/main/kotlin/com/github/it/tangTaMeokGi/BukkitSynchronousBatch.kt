64585d400184cda6274a4f1134927eeb7a45fdd6
it <yeongbin0909@naver.com>
2025-03-30 23:07:27 +0900

    작업 일괄 처리 객체 추가

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/BukkitSynchronousBatch.kt
@@ -0,0 +1,57 @@
+package com.github.it.tangTaMeokGi
+
+import kotlinx.coroutines.Runnable
+import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
+
+class BukkitSynchronousBatch(val plugin: Plugin, val timeOutMillis: Int) {
+    var isRunning: Boolean = false
+    val tasks: MutableList<Runnable> = mutableListOf()
+
+    var taskId: Int? = null
+
+    fun addTask(runnable: Runnable) {
+        tasks.add(runnable)
+    }
+
+    fun start() {
+        if (isRunning) {
+            throw RuntimeException("이미 실행중입니다")
+        }
+
+        isRunning = true
+
+        taskId = Bukkit.getScheduler().runTaskTimer(plugin,
+            Runnable { run() }, 1L, 1L
+        ).taskId
+    }
+
+    fun stop() {
+        if (!isRunning) {
+            throw RuntimeException("실행중이지 않습니다")
+        }
+
+        Bukkit.getScheduler().cancelTask(taskId!!)
+
+        isRunning = false
+    }
+
+    fun join() {
+        while (0 < tasks.size) {}
+    }
+
+    fun run() {
+        val startTime = System.currentTimeMillis()
+        val tasksArray = tasks.toList()
+        tasks.clear()
+
+
+        for (task in tasksArray) {
+            task.run()
+
+            if (timeOutMillis <= System.currentTimeMillis() - startTime) {
+                break
+            }
+        }
+    }
+}
