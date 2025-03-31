039d57ae03bcd735c611fbcb314d25a898416daa
it <yeongbin0909@naver.com>
2025-04-01 08:13:08 +0900

    이벤트 관련 어노테이션 추가

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/event/annotations.kt
@@ -0,0 +1,6 @@
+package com.github.it.tangTaMeokGi.core.event
+
+interface GameEventListener
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class GameEventHandler
