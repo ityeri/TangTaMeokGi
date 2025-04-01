fec9c710e4bb4e8b5e831e53187af76c6d303f9e
it <yeongbin0909@naver.com>
2025-04-01 23:06:05 +0900

    TODO 추거

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaState/WarGeneralAreaState.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaState/WarGeneralAreaState.kt
@@ -52,6 +52,7 @@ class WarGeneralAreaState(
     }
 
     override fun onAttack(areaAttackEvent: AreaAttackEvent) {
+        // TODO 공성전 지역에 점령 시도시 점령 불가 메세지 전송을 UserInterface 로 넘기기
         areaAttackEvent.attacker.sendMessage(
             "전쟁 영역에는 점령 시도를 할수 없습니다"
         )
