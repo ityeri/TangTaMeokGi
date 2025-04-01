9d19869cc85fd2c2fb2c4d644075d769f25a36e8
it <yeongbin0909@naver.com>
2025-04-01 19:11:26 +0900

    onAreaAttack, onPlayerEnterArea 매서드 구현 완료

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/UserInterface.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/UserInterface.kt
@@ -6,21 +6,50 @@ import com.github.it.tangTaMeokGi.core.area.areaState.OwnerbleAreaState
 import com.github.it.tangTaMeokGi.core.event.AreaAttackEvent
 import com.github.it.tangTaMeokGi.core.event.GameEventHandler
 import com.github.it.tangTaMeokGi.core.event.GameEventListener
+import com.github.it.tangTaMeokGi.core.event.PlayerEnterAreaEvent
 
 class UserInterface(val game: Game): GameEventListener {
     fun enable() {
         val commandManager = PaperCommandManager(game.plugin)
 
         commandManager.registerCommand(GetAreaInfoCommand(game))
+
+        game.eventDispatcher.register(this)
     }
 
     @GameEventHandler
     fun onAreaAttack(areaAttackEvent: AreaAttackEvent) {
-        when(areaAttackEvent.area.state) {
+        val area = areaAttackEvent.area
+        val state = area.state
+        val attacker = areaAttackEvent.attacker
+        val attackerTeam = areaAttackEvent.attackerTeam
+
+        when(state) {
             is OwnerbleAreaState -> {
-                (areaAttackEvent.area.state as OwnerbleAreaState)
-                    .ownerTeam.sendMessage("님 땅 ${areaAttackEvent.attackerTeam.displayName}팀의" +
-                            "${areaAttackEvent.attacker.name}라는 놈한테 공격맞음 ㅅㄱ")
+                state.ownerTeam.sendMessage(
+                    "님 팀의 x ${area.x * area.size} z ${area.z * area.size} 쪽 땅이 " +
+                    "${attackerTeam.displayName} 팀의 ${attacker.name} 라는 놈한테 공격맞음 ㅅㄱ")
+            }
+        }
+
+    }
+
+    @GameEventHandler
+    fun onPlayerEnterArea(playerEnterAreaEvent: PlayerEnterAreaEvent) {
+        val area = playerEnterAreaEvent.area
+        val state = area.state
+        val enteredPlayer = playerEnterAreaEvent.player
+        val enteredPlayerTeam = game.teamManager!!.getTeam(enteredPlayer)
+
+        enteredPlayerTeam.let {
+            when (state) {
+                is OwnerbleAreaState -> {
+                    val ownerTeam = state.ownerTeam
+                    ownerTeam.sendMessage(
+                        "님들 팀 x ${area.x * area.size} z ${area.z * area.size} 쪽 땅에 " +
+                        "${enteredPlayerTeam!!.displayName} 팀의 ${enteredPlayer.name} 이라는 새끼 침입함"
+                    )
+                }
             }
         }
 
