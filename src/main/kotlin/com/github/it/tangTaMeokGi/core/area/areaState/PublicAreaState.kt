b7c890020ce11372fd8d3204fa829d6f1c31df9b
it <yeongbin0909@naver.com>
2025-04-01 11:20:31 +0900

    AreaEffect 임포트 수정

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaState/PublicAreaState.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaState/PublicAreaState.kt
@@ -3,6 +3,7 @@ package com.github.it.tangTaMeokGi.core.area.areaState
 import com.github.it.tangTaMeokGi.core.team.Team
 import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.area.AreaType
+import com.github.it.tangTaMeokGi.core.area.areaState.EffectAreaState.AreaEffect
 import com.github.it.tangTaMeokGi.core.event.AttackEvent
 import org.bukkit.entity.Player
 
