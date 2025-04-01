54a543198317eaf07b58a230a8789ab8086b9c26
it <yeongbin0909@naver.com>
2025-04-02 08:14:48 +0900

    승리여부 확인 로직 추가

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/warAreaData/GeneralWarAreaData.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/warAreaData/GeneralWarAreaData.kt
@@ -6,6 +6,7 @@ import com.github.it.tangTaMeokGi.core.area.AreaType
 import com.github.it.tangTaMeokGi.core.area.areaData.commonAreaData.GeneralAreaData
 import com.github.it.tangTaMeokGi.core.event.AreaAttackEvent
 import com.github.it.tangTaMeokGi.core.event.WarEndEvent
+import org.bukkit.entity.Player
 
 class GeneralWarAreaData(
     area: Area,
@@ -23,10 +24,38 @@ class GeneralWarAreaData(
         get() = warEndTime - (System.currentTimeMillis()/1000).toInt()
 
 
+
     override fun onEnable() {
     }
 
     override fun onDisable() {
+    }
+
+
+
+    fun onWarEnd() {
+        var isAttackerWin = true
+
+        for (entity in area.getEntities()) {
+
+            when (entity) {
+                is Player -> {
+                    // ownerTeam (원래 주인팀) 소속의 플레이어가 한명이라도 있을경우
+                    // 공격자 승리 여부가 false 가 됨
+                    if (game.teamManager!!.getTeam(entity) == ownerTeam) {
+                        isAttackerWin = false
+                        break
+                    }
+                }
+            }
+
+        }
+
+        if (isAttackerWin) {
+            onAttackerTeamWin()
+        } else {
+            onOwnerTeamWin()
+        }
     }
 
     override fun onOwnerTeamWin() {
@@ -53,10 +82,12 @@ class GeneralWarAreaData(
         area.enable()
     }
 
+
+
     override fun update() {
         val currentTime = (System.currentTimeMillis()/1000).toInt()
         if (timeLeft <= 0) {
-            onAttackerTeamWin()
+            onWarEnd()
         }
     }
 
