69a682c8786b79fa88f1c3d382b0d75274e5433a
it <yeongbin0909@naver.com>
2025-04-01 16:40:56 +0900

    초기화 단계에서 ownerTeam 을 받도록 변경

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaState/OwnerbleAreaState.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaState/OwnerbleAreaState.kt
@@ -3,6 +3,5 @@ package com.github.it.tangTaMeokGi.core.area.areaState
 import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.team.Team
 
-abstract class OwnerbleAreaState(area: Area) : BaseAreaState(area) {
-    abstract val ownerTeam: Team
-}
+abstract class OwnerbleAreaState(area: Area, val ownerTeam: Team) : BaseAreaState(area)
+// TODO OwnerbleAreaState 설정 ㄱㄱ
