061ebb126fe4ac457cfe1c855cc5e8f2993aa5b4
it <yeongbin0909@naver.com>
2025-04-01 20:13:02 +0900

    BaseWarAreaState 추가

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaState/BaseWarAreaState.kt
@@ -0,0 +1,8 @@
+package com.github.it.tangTaMeokGi.core.area.areaState
+
+import com.github.it.tangTaMeokGi.core.area.Area
+import com.github.it.tangTaMeokGi.core.team.Team
+
+abstract class BaseWarAreaState(area: Area, ownerTeam: Team, val attackerTeam: Team,
+                                val warTime: Int):
+    OwnerbleAreaState(area, ownerTeam)
