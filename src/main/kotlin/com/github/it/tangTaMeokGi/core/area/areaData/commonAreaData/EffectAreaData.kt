25cfda2587274d8a7ba0ddee9366b95435c8e4bf
it <yeongbin0909@naver.com>
2025-04-02 09:24:54 +0900

    onAttack 구현

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/commonAreaData/EffectAreaData.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/commonAreaData/EffectAreaData.kt
@@ -4,31 +4,45 @@ import com.github.it.tangTaMeokGi.core.team.Team
 import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.area.AreaType
 import com.github.it.tangTaMeokGi.core.area.areaData.OwnerbleAreaData
+import com.github.it.tangTaMeokGi.core.area.areaData.warAreaData.EffectWarAreaData
 import com.github.it.tangTaMeokGi.core.event.AreaAttackEvent
+import com.github.it.tangTaMeokGi.core.event.WarStartEvent
 import org.bukkit.potion.PotionEffect
 
 open class EffectAreaData(area: Area, ownerTeam: Team,
-                          val areaEffect: AreaEffect
+                          val areaPotionEffect: AreaPotionEffect
 ) : OwnerbleAreaData(area, ownerTeam) {
 
-    data class AreaEffect(val buffEffect: PotionEffect? = null, val debuffEffect: PotionEffect? = null)
+    data class AreaPotionEffect(val buffEffect: PotionEffect? = null, val debuffEffect: PotionEffect? = null)
 
     override val type = AreaType.EFFECT_AREA
 
     override fun onEnable() {
-        TODO("이벤트 리스너 추가 코드")
     }
 
     override fun onDisable() {
-        TODO("이벤트 리스너 제거")
     }
 
     override fun update() {
-        TODO("이펙 넣는거 추가 ㄱ")
+        // TODO "이펙 넣는거 추가 ㄱ"
     }
 
     override fun onAttack(areaAttackEvent: AreaAttackEvent) {
-        TODO("Not yet implemented")
+        // 자기 팀에 자기가 공격 시도했을 경우
+        if (ownerTeam == areaAttackEvent.attackerTeam) {
+            areaAttackEvent.canceled = true
+            return
+        }
+
+        area.data = EffectWarAreaData(
+            area, ownerTeam, areaAttackEvent.attackerTeam, area.game.setting!!.warTime,
+            areaPotionEffect
+        )
+        area.enable()
+
+        area.game.eventDispatcher.callEvent(
+            WarStartEvent(area, ownerTeam, areaAttackEvent.attackerTeam)
+        )
     }
 
 }
