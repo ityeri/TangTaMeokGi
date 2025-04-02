f114f99075020e2e7c474368ebbb84e117092a60
it <yeongbin0909@naver.com>
2025-04-02 10:30:08 +0900

    불필요한 코드 제거

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/command/MapCommand.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/command/MapCommand.kt
@@ -7,6 +7,7 @@ import co.aikar.commands.annotation.Default
 import com.github.it.tangTaMeokGi.core.Game
 import com.github.it.tangTaMeokGi.core.area.areaData.OwnerbleAreaData
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 
 @CommandAlias("map")
@@ -16,27 +17,12 @@ class MapCommand(val game: Game) : BaseCommand() {
     @Default
     fun onCommand(sender: CommandSender) {
         when (sender) {
-            is Player -> {
-                val area = game.areaManager!!.getArea(sender)
+            is Entity -> {
 
-                area.let {
-                    area!!
-                    sender.sendMessage("""
-                        현재 영역 위치: [${area.x}, ${area.z}]
-                        영역 종류: ${area.type}
-                        영역 활성화 여부: ${area.isEnabled}
-                    """.trimIndent())
-                    if (area.data is OwnerbleAreaData) {
-                        sender.sendMessage("땅 주인팀: ${(area.data as OwnerbleAreaData).ownerTeam.id}")
-                    }
-                }
-                area ?: {
-                    sender.sendMessage("그 어떤 영역 안에도 있지 않습니다")
-                }
             }
 
             else -> {
-                sender.sendMessage("플레이어만 이 명령어 쓸수있음 ㅅㄱ")
+                sender.sendMessage("엔티티만 이 명령어 쓸수있음 ㅅㄱ")
             }
         }
     }
