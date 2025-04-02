c5b9b4a495177945a3f21f2b4de1efcb60198158
it <yeongbin0909@naver.com>
2025-04-02 10:27:02 +0900

    map 명령어 클래스 추가

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/command/MapCommand.kt
@@ -0,0 +1,43 @@
+package com.github.it.tangTaMeokGi.userInterface.command
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Default
+import com.github.it.tangTaMeokGi.core.Game
+import com.github.it.tangTaMeokGi.core.area.areaData.OwnerbleAreaData
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+@CommandAlias("map")
+@CommandPermission("op")
+class MapCommand(val game: Game) : BaseCommand() {
+
+    @Default
+    fun onCommand(sender: CommandSender) {
+        when (sender) {
+            is Player -> {
+                val area = game.areaManager!!.getArea(sender)
+
+                area.let {
+                    area!!
+                    sender.sendMessage("""
+                        현재 영역 위치: [${area.x}, ${area.z}]
+                        영역 종류: ${area.type}
+                        영역 활성화 여부: ${area.isEnabled}
+                    """.trimIndent())
+                    if (area.data is OwnerbleAreaData) {
+                        sender.sendMessage("땅 주인팀: ${(area.data as OwnerbleAreaData).ownerTeam.id}")
+                    }
+                }
+                area ?: {
+                    sender.sendMessage("그 어떤 영역 안에도 있지 않습니다")
+                }
+            }
+
+            else -> {
+                sender.sendMessage("플레이어만 이 명령어 쓸수있음 ㅅㄱ")
+            }
+        }
+    }
+}
