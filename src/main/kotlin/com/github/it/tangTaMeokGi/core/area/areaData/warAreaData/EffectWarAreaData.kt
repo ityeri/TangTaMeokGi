34d55fc09d94452c23da5863f84dfa1deca2bfee
it <yeongbin0909@naver.com>
2025-04-02 09:26:14 +0900

    생성자 수정, 공성전 종료 관련 로직 구현

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/warAreaData/EffectWarAreaData.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/warAreaData/EffectWarAreaData.kt
@@ -4,33 +4,89 @@ import com.github.it.tangTaMeokGi.core.team.Team
 import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.area.AreaType
 import com.github.it.tangTaMeokGi.core.area.areaData.commonAreaData.EffectAreaData
+import com.github.it.tangTaMeokGi.core.area.areaData.commonAreaData.EffectAreaData.AreaPotionEffect
+import com.github.it.tangTaMeokGi.core.area.areaData.commonAreaData.GeneralAreaData
 import com.github.it.tangTaMeokGi.core.event.AreaAttackEvent
+import com.github.it.tangTaMeokGi.core.event.WarEndEvent
+import org.bukkit.entity.Player
 
 class EffectWarAreaData(
     area: Area,
     ownerTeam: Team,
-    areaPotionEffect: AreaEffect,
-    val attackerTeam: Team,
-    val timeLimitSec: Int
-) : EffectAreaData(area, ownerTeam, areaPotionEffect) {
+    attackerTeam: Team,
+    timeLimitSec: Int,
+    val areaPotionEffect: AreaPotionEffect
+) : BaseWarAreaData(area, ownerTeam, attackerTeam, timeLimitSec) {
 
     override val type = AreaType.WAR_EFFECT_AREA
+    override var warEndTime: Int? = null
 
-    var timer: Int = timeLimitSec
 
     override fun onEnable() {
-        TODO("이벤트 리스너 추가 코드")
+        warEndTime = (System.currentTimeMillis()/1000).toInt() + warTime
     }
     override fun onDisable() {
-        TODO("이벤트 리스너 제거")
     }
+
+
+    fun onWarEnd() {
+        var isAttackerWin = true
+
+        for (entity in area.getEntities()) {
+
+            when (entity) {
+                is Player -> {
+                    // ownerTeam (원래 주인팀) 소속의 플레이어가 한명이라도 있을경우
+                    // 공격자 승리 여부가 false 가 됨
+                    if (game.teamManager!!.getTeam(entity) == ownerTeam) {
+                        isAttackerWin = false
+                        break
+                    }
+                }
+            }
+
+        }
+
+        if (isAttackerWin) {
+            onAttackerTeamWin()
+        } else {
+            onOwnerTeamWin()
+        }
+    }
+
+    override fun onOwnerTeamWin() {
+        area.game.eventDispatcher.callEvent(
+            WarEndEvent(
+                area, ownerTeam, attackerTeam, false
+            )
+        )
+        area.data = EffectAreaData(
+            area, ownerTeam, areaPotionEffect
+        )
+        area.enable()
+    }
+
+    override fun onAttackerTeamWin() {
+        area.game.eventDispatcher.callEvent(
+            WarEndEvent(
+                area, ownerTeam, attackerTeam, true
+            )
+        )
+        area.data = EffectAreaData(
+            area, attackerTeam, areaPotionEffect
+        )
+        area.enable()
+    }
+
 
     override fun update() {
-        TODO("Not yet implemented")
+        if (timeLeft <= 0) {
+            onWarEnd()
+        }
     }
-
     override fun onAttack(areaAttackEvent: AreaAttackEvent) {
-        TODO("전장으로 들어오는 공격 이밴트는 씹어야함")
+        // 공성전이 진행중인 땅에 공격을 할순 없음
+        areaAttackEvent.canceled = true
     }
 
 }
