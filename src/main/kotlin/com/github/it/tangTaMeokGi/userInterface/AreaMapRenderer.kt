d73f0608be0e14c50559b9a8013822cc69a476ad
it <yeongbin0909@naver.com>
2025-04-02 18:52:52 +0900

    지원 중단된 매서드 교체

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/AreaMapRenderer.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/AreaMapRenderer.kt
@@ -1,20 +1,25 @@
 package com.github.it.tangTaMeokGi.userInterface
 
+import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.area.AreaManager
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.server.MapInitializeEvent
 import org.bukkit.map.*
+import java.awt.Color
 
-class AreaMapRenderer(areaManager: AreaManager): MapRenderer() {
+class AreaMapRenderer(areaManager: AreaManager): MapRenderer(), Listener {
     override fun render(mapView: MapView, canvas: MapCanvas, player: Player) {
-        println("render 호출")
-        // 지도에 원하는 내용 그리기 (텍스트, 색상, 이미지 등)
-        canvas.drawText(10, 10, MinecraftFont.Font, "Error!")
+        canvas.drawText(10, 10, MinecraftFont.Font, "Test!")
 
         // 특정 위치에 색칠 (픽셀 단위)
         for (x in 30..50) {
             for (y in 30..50) {
-                canvas.setPixel(x, y, MapPalette.RED)
+                canvas.setPixelColor(x, y, Color.RED)
             }
         }
     }
+
+
 }
