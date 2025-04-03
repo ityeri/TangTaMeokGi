a628ceba3583711a054756bfc72d8806b8d9d8c8
it <yeongbin0909@naver.com>
2025-04-04 07:59:24 +0900

    컨텍스트를 true 로 설정

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/AreaMapRenderer.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/AreaMapRenderer.kt
@@ -4,24 +4,22 @@ import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.area.AreaManager
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
 import org.bukkit.event.server.MapInitializeEvent
 import org.bukkit.map.*
 import java.awt.Color
 
-class AreaMapRenderer(areaManager: AreaManager): MapRenderer(), Listener {
+class AreaMapRenderer(areaManager: AreaManager): MapRenderer(true) {
     override fun render(mapView: MapView, canvas: MapCanvas, player: Player) {
         canvas.drawText(10, 10, MinecraftFont.Font, "Test!")
 
         // 특정 위치에 색칠 (픽셀 단위)
         for (x in 30..50) {
             for (y in 30..50) {
-                canvas.setPixelColor(x, y, Color.RED)
+                canvas.setPixelColor(x, y, Color.WHITE)
             }
         }
 
         player.sendMessage("AreaManager.render")
     }
-
 
 }
