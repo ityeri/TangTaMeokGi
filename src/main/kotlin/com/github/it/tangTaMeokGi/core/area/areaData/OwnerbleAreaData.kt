703f2a17ccff4458383fce5e49842f9d7f5a3d10
it <yeongbin0909@naver.com>
2025-04-10 17:14:35 +0900

    area 의 주인팀을 즉시 간단하게 변경할수 있는 추상 매서드 구현

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/OwnerbleAreaData.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/OwnerbleAreaData.kt
@@ -3,4 +3,6 @@ package com.github.it.tangTaMeokGi.core.area.areaData
 import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.team.Team
 
-abstract class OwnerbleAreaData(area: Area, val ownerTeam: Team) : BaseAreaData(area)
+abstract class OwnerbleAreaData(area: Area, val ownerTeam: Team) : BaseAreaData(area) {
+    abstract fun changeOwner(team: Team)
+}
