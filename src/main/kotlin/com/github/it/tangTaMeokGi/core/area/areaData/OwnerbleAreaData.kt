de8c9494e1a8cf92d1e24becb3a767ff13126c9b
it <yeongbin0909@naver.com>
2025-04-10 17:17:07 +0900

    changeOwner 에서 setOwner 로 리펙터링

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/OwnerbleAreaData.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/OwnerbleAreaData.kt
@@ -4,5 +4,5 @@ import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.team.Team
 
 abstract class OwnerbleAreaData(area: Area, val ownerTeam: Team) : BaseAreaData(area) {
-    abstract fun changeOwner(team: Team)
+    abstract fun setOwner(team: Team)
 }
