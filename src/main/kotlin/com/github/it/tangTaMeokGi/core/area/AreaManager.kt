2486495d0788620104d87394457378e3f01cb1c7
it <yeongbin0909@naver.com>
2025-04-11 10:45:14 +0900

    플러드필 코드만 개별 매서드로 분리

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/AreaManager.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/AreaManager.kt
@@ -9,6 +9,7 @@ import com.github.it.tangTaMeokGi.core.area.areaData.warAreaData.BaseWarAreaData
 import com.github.it.tangTaMeokGi.core.event.AreaOccupationEvent
 import com.github.it.tangTaMeokGi.core.event.GameEventHandler
 import com.github.it.tangTaMeokGi.core.event.GameEventListener
+import com.github.it.tangTaMeokGi.core.team.Team
 import org.bukkit.Material
 import org.bukkit.World
 import org.bukkit.inventory.ItemStack
@@ -199,88 +200,19 @@ class AreaManager(
     fun onAreaOccupation(event: AreaOccupationEvent) {
 
         val team = event.winningTeam
-//        val totalSearchedAreas: MutableSet<Area> = mutableSetOf()
+        // val totalSearchedAreas: MutableSet<Area> = mutableSetOf()
         val adjacentOffsets: List<List<Int>> = listOf(
             listOf(-1, 0), listOf(1, 0), listOf(0, -1), listOf(0, 1)
         )
 
-        for (seedArea in getAllArea()) run seedLoop@{
-            // 순수 울팀 땅이면
-            // 그니깐, OwnerbleAreaData 이면서, BaseWarAreaData 가 아니면서
-            // 주인 팀이 점령 시도 팀과 동일할경우
-            if (seedArea.data is OwnerbleAreaData && seedArea.data !is BaseWarAreaData
-                && (seedArea.data as OwnerbleAreaData).ownerTeam == team) {
-                // 암것도 안하고 다른 영역을 시드로 한 플러드필로 건너뜀
-                return@seedLoop
-            }
+        for (seedArea in getAllArea()) {
 
-            var currentSearchingAreas = mutableSetOf(seedArea)
-            val nextSearchingAreas = mutableSetOf<Area>()
+            val areas = checkCloseSpace(seedArea.x, seedArea.z, team)
 
-            // allFinedAreas 와 totalSearchedAreas 햇갈리면 안됨
-            // allFinedAreas 는 현재 시드로부터 찾아진 모든 땅이고,
-            // totalSearchedAreas 는 현재 탐색한 모든 시드에 대해 찾아진 땅이 저장되는 곳임
-            val allFinedAreas = mutableSetOf(seedArea)
+            areas ?: run { return }
 
-            var isClosed = true
-
-            // 플러드필 루프 하나 (마름모 한칸)
-            while (true) {
-                // 현재 확인하는 모든 area 를 하나하나 순회 (겉면에 둘러져 있는 땅들)
-                for (area in currentSearchingAreas) {
-
-                    // area 와 인접한 상하좌우 영역 확인
-                    for (adjacentOffset in adjacentOffsets) {
-
-                        val adjacentArea = getArea(
-                            area.x + adjacentOffset[0],
-                            area.z + adjacentOffset[0]
-                        )
-
-                        // getArea 를 통해 찾은 adjacentArea 가 null 라는건,
-                        // 해당 공간이 닫혀있지 않고, 벽끝까지 닿아있단 의미
-                        adjacentArea ?: run { isClosed = false }
-                        if (!isClosed) {
-                            return@seedLoop
-                        }
-
-                        val areaData = adjacentArea!!.data
-
-                        // 현재 탐색하는 인접한 땅이 현재 시드로 부터 찾은 땅에 이미 포함되 있으면 건너뛰기
-                        if (adjacentArea in allFinedAreas) { continue }
-
-                        // 순수 울팀 땅이면
-                        // 그니깐, OwnerbleAreaData 이면서, BaseWarAreaData 가 아니면서
-                        // 주인 팀이 점령 시도 팀과 동일할경우
-                        if (areaData is OwnerbleAreaData && areaData !is BaseWarAreaData
-                            && areaData.ownerTeam == team) {
-                            // 암것도 하지 않고 다음 인접땅 탐색으로 건너 뜀
-                            continue
-                        }
-                        // 순수 울팀땅이 아니면 (전쟁지역도 포함)
-                        else {
-                            // 현재 시드로 부터 찾아진 모든 땅에 이 인접땅 추가
-                            allFinedAreas += adjacentArea
-                            // 다음에 탐색할 땅 목록에 이 인접땅 추가
-                            nextSearchingAreas += adjacentArea
-                        }
-                    }
-
-                }
-
-                // 탐색 가능한 모든 공간을 찾아서 다음에 확인할 영역이 없을시
-                if (nextSearchingAreas.isEmpty()) {
-                    break
-                }
-                else {
-                    currentSearchingAreas = nextSearchingAreas
-                    nextSearchingAreas.clear()
-                }
-
-            }
-
-            if (isClosed) {
-                for (area in allFinedAreas) {
+            areas.let {
+                for (area in areas) {
                     val areaData = area.data
 
                     when (areaData) {
@@ -302,5 +234,85 @@ class AreaManager(
             }
         }
     }
-    
+
+    fun checkCloseSpace(seedX: Int, seedY: Int, team: Team): Set<Area>? {
+        val seedArea = getArea(seedX, seedY)!!
+        val adjacentOffsets: List<List<Int>> = listOf(
+            listOf(-1, 0), listOf(1, 0), listOf(0, -1), listOf(0, 1)
+        )
+
+        // 순수 울팀 땅이면
+        // 그니깐, OwnerbleAreaData 이면서, BaseWarAreaData 가 아니면서
+        // 주인 팀이 점령 시도 팀과 동일할경우
+        if (seedArea.data is OwnerbleAreaData && seedArea.data !is BaseWarAreaData
+            && (seedArea.data as OwnerbleAreaData).ownerTeam == team) {
+            // 암것도 안하고 다른 영역을 시드로 한 플러드필로 건너뜀
+            return null
+        }
+
+        var currentSearchingAreas = mutableSetOf(seedArea)
+        val nextSearchingAreas = mutableSetOf<Area>()
+
+        // allFinedAreas 는 현재 시드로부터 찾아진 모든 땅
+        val allFinedAreas = mutableSetOf(seedArea)
+
+        var isClosed = true
+
+        // 플러드필 루프 하나 (마름모 한겹)
+        while (true) {
+            // 현재 확인하는 모든 area 를 하나하나 순회 (겉면에 둘러져 있는 땅들)
+            for (area in currentSearchingAreas) {
+
+                // area 와 인접한 상하좌우 영역 확인
+                for (adjacentOffset in adjacentOffsets) {
+
+                    val adjacentArea = getArea(
+                        area.x + adjacentOffset[0],
+                        area.z + adjacentOffset[1]
+                    )
+
+                    // getArea 를 통해 찾은 adjacentArea 가 null 라는건,
+                    // 해당 공간이 닫혀있지 않고, 벽끝까지 닿아있단 의미
+                    adjacentArea ?: run { isClosed = false }
+                    if (!isClosed) {
+                        return null
+                    }
+
+                    val areaData = adjacentArea!!.data
+
+                    // 현재 탐색하는 인접한 땅이 현재 시드로 부터 찾은 땅에 이미 포함되 있으면 건너뛰기
+                    if (adjacentArea in allFinedAreas) { continue }
+
+                    // 순수 울팀 땅이면
+                    // 그니깐, OwnerbleAreaData 이면서, BaseWarAreaData 가 아니면서
+                    // 주인 팀이 점령 시도 팀과 동일할경우
+                    if (areaData is OwnerbleAreaData && areaData !is BaseWarAreaData
+                        && areaData.ownerTeam == team) {
+                        // 암것도 하지 않고 다음 인접땅 탐색으로 건너 뜀
+                        continue
+                    }
+                    // 순수 울팀땅이 아니면 (전쟁지역도 포함)
+                    else {
+                        // 현재 시드로 부터 찾아진 모든 땅에 이 인접땅 추가
+                        allFinedAreas += adjacentArea
+                        // 다음에 탐색할 땅 목록에 이 인접땅 추가
+                        nextSearchingAreas += adjacentArea
+                    }
+                }
+
+            }
+
+            // 탐색 가능한 모든 공간을 찾아서 다음에 확인할 영역이 없을시
+            if (nextSearchingAreas.isEmpty()) {
+                break
+            }
+            else {
+                currentSearchingAreas = nextSearchingAreas
+                nextSearchingAreas.clear()
+            }
+
+        }
+
+        return allFinedAreas
+    }
 }
