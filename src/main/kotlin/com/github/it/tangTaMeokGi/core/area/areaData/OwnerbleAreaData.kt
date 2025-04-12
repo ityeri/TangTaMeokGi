f86e7592236a22515eb978c64c70198dd0f012d3
it <yeongbin0909@naver.com>
2025-04-12 19:18:44 +0900

    OwnerbleAreaData 가 OccupiableArea 를 상속하도록 변경

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/OwnerbleAreaData.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/OwnerbleAreaData.kt
@@ -3,6 +3,4 @@ package com.github.it.tangTaMeokGi.core.area.areaData
 import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.team.Team
 
-abstract class OwnerbleAreaData(area: Area, val ownerTeam: Team) : BaseAreaData(area) {
-    abstract fun setOwner(team: Team)
-}
+abstract class OwnerbleAreaData(area: Area, val ownerTeam: Team) : OccupiableArea(area)
