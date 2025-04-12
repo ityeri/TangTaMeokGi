7cedb1725481f5c56c6bc3effa4c95b263e5307b
it <yeongbin0909@naver.com>
2025-04-12 19:34:26 +0900

    PublicAreaData 가 OccupiableAreaData 를 상속하도록 하고, occupyBy 구현

--- src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/commonAreaData/PublicAreaData.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/core/area/areaData/commonAreaData/PublicAreaData.kt
@@ -3,11 +3,14 @@ package com.github.it.tangTaMeokGi.core.area.areaData.commonAreaData
 import com.github.it.tangTaMeokGi.core.area.Area
 import com.github.it.tangTaMeokGi.core.area.AreaType
 import com.github.it.tangTaMeokGi.core.area.areaData.BaseAreaData
+import com.github.it.tangTaMeokGi.core.area.areaData.OccupiableAreaData
 import com.github.it.tangTaMeokGi.core.area.areaData.commonAreaData.EffectAreaData.AreaPotionEffect
 import com.github.it.tangTaMeokGi.core.event.AreaOccupationEvent
 import com.github.it.tangTaMeokGi.core.event.AreaAttackEvent
+import com.github.it.tangTaMeokGi.core.team.Team
+import org.bukkit.entity.Player
 
-class PublicAreaData(area: Area, var isEffectArea: Boolean, val potionEffect: AreaPotionEffect? = null) : BaseAreaData(area) {
+class PublicAreaData(area: Area, var isEffectArea: Boolean, val potionEffect: AreaPotionEffect? = null) : OccupiableAreaData(area) {
 
     override val type = AreaType.PUBLIC_AREA
 
@@ -18,26 +21,29 @@ class PublicAreaData(area: Area, var isEffectArea: Boolean, val potionEffect: Ar
         // TODO potionEffect 로 해당 영역 내에 플레ㅔ이어 한테 효과 넣는거 구현 ㄱ
     }
 
-
-    override fun onAttack(event: AreaAttackEvent) {
-
+    override fun occupyBy(team: Team, attacker: Player, callEvent: Boolean) {
         if (isEffectArea) {
             area.data = EffectAreaData(
-                area, event.attackerTeam, potionEffect!!
+                area, team, potionEffect!!
             )
         }
         else {
             area.data = GeneralAreaData(
-                area, event.attackerTeam
+                area, team
             )
         }
 
-        area.game.eventDispatcher.callEvent(
-            AreaOccupationEvent(area, null,
-                event.attackerTeam,
-                event.attacker)
-        )
+        if (callEvent) {
+            area.game.eventDispatcher.callEvent(
+                AreaOccupationEvent(area, null,
+                    team, attacker)
+            )
+        }
+    }
 
+
+    override fun onAttack(event: AreaAttackEvent) {
+        occupyBy(event.attackerTeam, event.attacker)
         area.enable()
     }
 
