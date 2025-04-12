679f8ac3c381db72b9d16c3d676fca27db8c12b7
it <yeongbin0909@naver.com>
2025-04-12 20:05:35 +0900

    점령 이벤트 발생시 점령을 시도한 플레이어가 null 일시 메세지 보내지 않음

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/UserInterface.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/UserInterface.kt
@@ -56,7 +56,7 @@ class UserInterface(val game: Game, val scope: CoroutineScope): GameEventListene
         val area = areaOccupationEvent.area
 
         areaOccupationEvent.attacker.let {
-            areaOccupationEvent.attacker!!.sendMessage(
+            areaOccupationEvent.attacker?.sendMessage(
                 "x ${area.x * area.size} z ${area.z * area.size} 쪽 땅 점령 성공함 =)"
             )
         }
