1a72f64e5e1c7d6168a20fbf8b2b8227b9aa4695
it <yeongbin0909@naver.com>
2025-04-15 07:42:49 +0900

    enable 매서드 추가, @GameEventHandler 어노테이션 추가

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/GameTimeBarManager.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/GameTimeBarManager.kt
@@ -1,6 +1,8 @@
 package com.github.it.tangTaMeokGi.userInterface
 
 import com.github.it.tangTaMeokGi.core.Game
+import com.github.it.tangTaMeokGi.core.event.GameEventHandler
+import com.github.it.tangTaMeokGi.core.event.GameEventListener
 import com.github.it.tangTaMeokGi.core.event.GameStartEvent
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
@@ -10,7 +12,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
-class GameTimeBarManager(val game: Game) : Listener {
+class GameTimeBarManager(val game: Game) : Listener, GameEventListener {
 
     val gameTimeBarNamespacedKey = NamespacedKey(game.plugin, "game_time_bar")
     val gameTimeBar = Bukkit.createBossBar(
@@ -18,10 +20,15 @@ class GameTimeBarManager(val game: Game) : Listener {
         "남은 겜시간", BarColor.YELLOW, BarStyle.SOLID
     )
 
+    fun enable() {
+        game.eventDispatcher.register(this)
+    }
+
     fun disable() {
         Bukkit.removeBossBar(gameTimeBarNamespacedKey)
     }
 
+    @GameEventHandler
     fun onGameStart(event: GameStartEvent) {
         Bukkit.getServer().onlinePlayers.forEach { player ->
             gameTimeBar.addPlayer(player)
