88a9d4c898017cba8b791bbb0852194e6079e226
it <yeongbin0909@naver.com>
2025-04-15 07:39:55 +0900

    game 에 val 키워드 추가, update 매서드와 PlayerJoinEvent 처리 추가, disable 시 보스바 제거 코드 추가

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/GameTimeBarManager.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/GameTimeBarManager.kt
@@ -6,8 +6,11 @@ import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.boss.BarColor
 import org.bukkit.boss.BarStyle
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 
-class GameTimeBarManager(game: Game) {
+class GameTimeBarManager(val game: Game) : Listener {
 
     val gameTimeBarNamespacedKey = NamespacedKey(game.plugin, "game_time_bar")
     val gameTimeBar = Bukkit.createBossBar(
@@ -15,9 +18,23 @@ class GameTimeBarManager(game: Game) {
         "남은 겜시간", BarColor.YELLOW, BarStyle.SOLID
     )
 
+    fun disable() {
+        Bukkit.removeBossBar(gameTimeBarNamespacedKey)
+    }
+
     fun onGameStart(event: GameStartEvent) {
         Bukkit.getServer().onlinePlayers.forEach { player ->
             gameTimeBar.addPlayer(player)
         }
+    }
+
+    fun update() {
+        val gameTimeLeftPercent = game.gameTimeLeft / game.setting!!.totalGameTime
+        gameTimeBar.progress = gameTimeLeftPercent.coerceIn(0.0, 1.0)
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        gameTimeBar.addPlayer(event.player)
     }
 }
