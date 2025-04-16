f0c0055a7ddb3ffbd90f712a30f28a754f0f5521
it <yeongbin0909@naver.com>
2025-04-17 01:22:16 +0900

    EnableableCommand 상속 추가

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/command/TeamCheckCommand.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/command/TeamCheckCommand.kt
@@ -1,7 +1,11 @@
 package com.github.it.tangTaMeokGi.userInterface.command
 
 import co.aikar.commands.annotation.CommandAlias
+import com.github.it.tangTaMeokGi.core.Game
 
 @CommandAlias("teams")
-class TeamCheckCommand {
+class TeamCheckCommand(val game: Game) : EnableableCommand() {
+    override fun enable() {
+
+    }
 }
