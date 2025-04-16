4a314008089e6c4d1c0e0ef8209e0a39924ff2e0
it <yeongbin0909@naver.com>
2025-04-17 01:05:34 +0900

    재사용성을 위해 컬러와 이름간 관계를 정의하는 열거형 클래스 추가

---
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/NamedColor.kt
@@ -0,0 +1,34 @@
+package com.github.it.tangTaMeokGi.userInterface
+
+import java.awt.Color
+
+enum class NamedColor(name: String, color: Color) {
+    BLACK("black", Color.BLACK),
+    WHITE("white", Color.WHITE),
+    RED("red", Color.RED),
+    GREEN("green", Color.GREEN),
+    BLUE("blue", Color.BLUE),
+    YELLOW("yellow", Color.YELLOW),
+    ORANGE("orange", Color.ORANGE),
+    PINK("pink", Color.PINK),
+    CYAN("cyan", Color.CYAN),
+    MAGENTA("magenta", Color.MAGENTA),
+    GRAY("gray", Color.GRAY),
+    LIGHT_GRAY("lightgray", Color.LIGHT_GRAY),
+    DARK_GRAY("darkgray", Color.DARK_GRAY)
+    ;
+
+    fun getColorFromName(name: String) : NamedColor? {
+        for (namedColor in entries) {
+            if (namedColor.name == name) {
+                return namedColor
+            }
+        }
+
+        return null
+    }
+
+    fun getColorNames() : List<String> {
+        return entries.map { namedColor -> namedColor.name }
+    }
+}
