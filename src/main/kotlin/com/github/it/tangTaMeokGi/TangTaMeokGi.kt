fbbf2b5dca00794b4b42386ac16cdead8315c4b3
it <yeongbin0909@naver.com>
2025-04-17 01:42:29 +0900

    테스트 코드 제거

--- src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/TangTaMeokGi.kt
@@ -20,11 +20,6 @@ class TangTaMeokGi : JavaPlugin(), GameEventListener {
     val game = Game(this)
     val userInterface = UserInterface(game, pluginScope)
 
-    lateinit var testPlayer1: Player
-    lateinit var testPlayer2: Player
-    lateinit var testTeam1: Team
-    lateinit var testTeam2: Team
-
 
     override fun onEnable() {
         Bukkit.getServer().sendMessage(Component.text("탕타묵기"))
@@ -37,18 +32,6 @@ class TangTaMeokGi : JavaPlugin(), GameEventListener {
         )
 
         game.areaTypeGenerate(0.3)
-
-        testTeam1 = Team(game, "test1", "test1", Color.RED)
-        testTeam2 = Team(game, "test2", "test2", Color.BLUE)
-
-        game.teamManager!!.addTeam(testTeam1)
-        game.teamManager!!.addTeam(testTeam2)
-
-        testPlayer1 = Bukkit.getServer().getPlayer("ityeri")!!
-        testPlayer2 = Bukkit.getServer().getPlayer("ritiey")!!
-
-        game.teamManager!!.getTeam("test1")!!.addPlayer(testPlayer1)
-        game.teamManager!!.getTeam("test2")!!.addPlayer(testPlayer2)
 
         userInterface.enable()
 
