16b0e8eaa16a599de4a09fe0d5f5e84903c4ac47
it <yeongbin0909@naver.com>
2025-04-17 01:16:49 +0900

    NamedColor 를 사용하도록 변경

--- src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/command/NewTeamCommand.kt
+++ src/main/kotlin/com/github/it/tangTaMeokGi/userInterface/command/NewTeamCommand.kt
@@ -7,6 +7,7 @@ import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Values
 import com.github.it.tangTaMeokGi.core.Game
 import com.github.it.tangTaMeokGi.core.team.Team
+import com.github.it.tangTaMeokGi.userInterface.NamedColor
 import org.bukkit.command.CommandSender
 import java.awt.Color
 
@@ -16,26 +17,10 @@ class NewTeamCommand(val game: Game) : EnableableCommand() {
 
     val teamManager = game.teamManager
 
-    val colorNameMap: Map<String, Color> = mapOf(
-        "black" to Color.BLACK,
-        "white" to Color.WHITE,
-        "red" to Color.RED,
-        "green" to Color.GREEN,
-        "blue" to Color.BLUE,
-        "yellow" to Color.YELLOW,
-        "orange" to Color.ORANGE,
-        "pink" to Color.PINK,
-        "cyan" to Color.CYAN,
-        "magenta" to Color.MAGENTA,
-        "gray" to Color.GRAY,
-        "lightgray" to Color.LIGHT_GRAY,
-        "darkgray" to Color.DARK_GRAY,
-    )
-
     override fun enable() {
         val commandManager = PaperCommandManager(game.plugin)
         commandManager.commandCompletions.registerCompletion("color") {
-            return@registerCompletion colorNameMap.keys
+            return@registerCompletion NamedColor.getColorNames()
         }
 
         commandManager.registerCommand(NewTeamCommand(game))
@@ -44,13 +29,16 @@ class NewTeamCommand(val game: Game) : EnableableCommand() {
     @Default
     fun onCommand(sender: CommandSender, teamId: String,
                   @Values("color") teamColorName: String) {
-        val teamColor = colorNameMap.get(teamColorName)
 
-        teamColor ?: run {
+        teamManager!!
+
+        val namedColor = NamedColor.getColorFromName(name)
+        namedColor ?: run {
             sender.sendMessage("색상 이름이 잘못되었습니다!")
         }
-        teamColor!!
-        teamManager!!
+        namedColor!!
+
+        val teamColor = namedColor.color
 
         val newTeam = Team(game, teamId, "test", teamColor)
 
